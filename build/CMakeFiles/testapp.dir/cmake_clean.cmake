ec06375c0a8b89950b052e092e9f88b66dc3c7c5
Ricardo Gomes <ricardo.gomes@skidata.com>
2018-05-06 20:37:12 +0200

    WIP: Added build folder for docker integration

---
+++ build/CMakeFiles/testapp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testapp.dir/src/helper.cpp.o"
+  "CMakeFiles/testapp.dir/src/main.cpp.o"
+  "testapp.pdb"
+  "testapp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
